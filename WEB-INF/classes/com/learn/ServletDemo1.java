3d65c6b1f9aa595f9320f7cf4eb45ae327bf6391
muke123com <muke123com@163.com>
2019-09-11 17:28:23 +0800

    基础

---
+++ WEB-INF/classes/com/learn/ServletDemo1.java
@@ -0,0 +1,36 @@
+package com.learn;
+
+/**
+ * Created by Administrator on 2017/12/21.
+ */
+
+import javax.servlet.*;
+import java.io.*;
+
+public class ServletDemo1 implements Servlet{
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- src/learn/FileDemo.java
+++ src/learn/FileDemo.java
@@ -1,17 +1,39 @@
 package learn;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by Administrator on 2017/12/20.
  */
 public class FileDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        File txt = new File("E:/Download/f1.txt");
+        FileOutputStream fos = new FileOutputStream(txt);
+        OutputStreamWriter osw = new OutputStreamWriter(fos, "utf-8");
+        BufferedWriter bw = new BufferedWriter(osw);
+
+        printFiles(new File("F:\\BaiduNetdiskDownload\\Java语言学习"), 0, bw);
+
+        bw.close();
+        osw.close();
+        fos.close();
+        System.out.println("写入完成");
+        System.exit(1);
+
         File file = new File("E:/Download/f.txt");
         if(file.exists()){
-            System.out.println(file.isFile());
-            System.out.println(file.isDirectory());
+//            System.out.println("文件大小" + (float)file.length()/1000 + "kb");
+//            System.out.println("文件可写" + file.canWrite());
+
+            System.out.println(file.setReadable(true));
+            System.out.println(file.setWritable(true));
+            System.out.println("文件可读" + file.canRead());
+
+//            System.out.println(file.isFile());
+//            System.out.println(file.isDirectory());
+//            File nameTo = new File("E:/Download/f1.txt");
+//            file.renameTo(nameTo);
         }else{
             System.out.println("文件不存在");
             try {
@@ -20,6 +42,36 @@ public class FileDemo {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+        //文件夹
+//        File folder = new File("E:/Download/bin");
+//        if(folder.exists()){
+//            System.out.println("文件夹已存在！");
+//        }else{
+//            folder.mkdir();
+//            System.out.println("文件夹创建成功！");
+//        }
+
+    }
+
+    //遍历文件
+    public static void printFiles(File dir, int tab, BufferedWriter bw) throws IOException {
+        String str = "";
+        if(dir.isDirectory()){
+
+            File[] next = dir.listFiles();
+            for (File f:next) {
+                for (int i=0;i<tab;i++){
+                    System.out.print("---");
+                    bw.write("---");
+                }
+                System.out.println(f.getName());
+                bw.write(f.getName() + "\n");
+                if(f.isDirectory()){
+                    printFiles(f, tab + 1, bw);
+                }
+            }
+
         }
     }
 }
